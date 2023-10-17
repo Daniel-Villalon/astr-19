3b6451cf21eb504e5bc2b6c92c83ac312572cc3a
Daniel-Villalon <dannyvillalon2003@gmail.com>
2023-10-16 20:16:39 -0700

    removing some files

--- journal-1-1.py
+++
@@ -1,9 +0,0 @@
-def PrintHelloWorld():
-	print('My name is Daniel Villalon Mendoza')
-	print('My favorite movie is Interstellar because I love space stuff. My favorite food is pizza, I love pizza!  ')
-
-def main():
-	PrintHelloWorld()
-
-if __name__=="__main__":
-	main() 
